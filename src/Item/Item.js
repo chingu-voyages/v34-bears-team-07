a40ffdfaabce43aea85a8a4ee8536751d98a18b8
Ryan <leeyuenhigh@gmail.com>
2021-10-29 19:57:32 -0400

    Fixed item commonet so that it can render data from backend

--- src/Item/Item.js
+++ src/Item/Item.js
@@ -1,45 +1,39 @@
-import React from 'react';
+import React from "react";
 
 function Item(props) {
-    // let image = "../../images/no-image.jpg"
-    // if (props.item.coffeeImage !== '') {
-    //     image = props.item.coffeeImage
-    // }
-        
-    return(
-        <section className='form-section'>
-            <div className='group'>
-                {/* <div className='item'>
-                    <img src={image} alt=''/>
-                </div> */}
-                <div className='item'>
-                    <h2>{props.item.name}</h2>                
-                </div>
-                <div className='item'>
-                    <h3>
-                        DOP 
-                        <p className='center'> {props.item.puchaseDate}</p>
-                    </h3>
-                </div>
-                <div className='item'>
-                    <h3>
-                        EXP 
-                        <p className='center'>{props.item.expirationDate}</p>
-                    </h3>                    
-                </div>
-                <div className='item'>
-                    <h3>
-                        QTY 
-                        <p className='center'>{props.item.quantity}</p>
-                    </h3>                    
-                </div>
-                <div className='remove item '>          
-                    <button type="button"><i className="fa fa-minus"></i></button>
-                </div>
-            </div>
-            <br></br>           
-        </section>
-    )
+  return (
+    <section className="form-section">
+      <div className="group">
+        <div className="item">
+          <h2>{props.item.name}</h2>
+        </div>
+        <div className="item">
+          <h3>
+            DOP
+            <p className="center"> {props.item.purchaseDate}</p>
+          </h3>
+        </div>
+        <div className="item">
+          <h3>
+            EXP
+            <p className="center">{props.item.expireDate}</p>
+          </h3>
+        </div>
+        <div className="item">
+          <h3>
+            QTY
+            <p className="center">{props.item.qty}</p>
+          </h3>
+        </div>
+        <div className="remove item ">
+          <button type="button">
+            <i className="fa fa-minus"></i>
+          </button>
+        </div>
+      </div>
+      <br></br>
+    </section>
+  );
 }
 
 export default Item;
