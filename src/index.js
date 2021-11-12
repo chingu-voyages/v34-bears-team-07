d9df2e7e1c32c03afd21da255167e6a0aa5a34e9
newspaperhobo <hoboinabox@gmail.com>
2021-11-11 20:40:16 -0500

    adds bootstrap and padding to bottom of landing page imgs

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
+import "./bootstrap.min.css";
 import { createStore, applyMiddleware } from "redux";
 import { Provider } from "react-redux";
 import thunk from "redux-thunk";
